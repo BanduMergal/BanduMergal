bd99a2c1e9a62acbb1d3118dd4c56376860eb716
BanduMergal <82564084+BanduMergal@users.noreply.github.com>
2022-10-19 20:50:44 +0530

    Git.cpp
    I am a new learner with PW skills.
    so errors in my code that you see clear it
    Thanks!

---
+++ Git.cpp
@@ -0,0 +1,13 @@
+include <iostream>
+int main(){
+   
+   int x=3;
+   int y=4;
+   
+   cout<< "x" << x <<endl;
+   cout<< "y" << y <<endl;
+   cout<< "Area of Rectangle"<< Area is = <<endl;
+   cout<< "Area of Rectangle"<< x*y <<endl;
+   
+   return 0;
+}
